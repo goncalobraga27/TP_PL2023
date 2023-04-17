063b760e32faa20156b278452aa79c9a8d9c6bd0
goncalobraga27 <a97541@alunos.uminho.pt>
2023-04-17 17:51:28 +0100

    Alterações na main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     conv = Conversor()
     f = open('dataset.txt')
     data = f.read()
-    conv.conversor(str(data))
+    data = conv.splitData(data)
+    conv.conversor(data)
 if __name__ == "__main__":
     main()
