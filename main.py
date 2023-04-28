e4a263919e1d7d2f94373aec2e0e2a2f48cb316e
goncalobraga27 <a97541@alunos.uminho.pt>
2023-04-28 17:32:16 +0100

    Menu iterativo

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import toml
 from conversorTOMLtoJSON import Conversor
 def main():
-    conv = Conversor()
-    f = open('dataset.txt')
-    data = f.read()
-    data = conv.splitData(data)
-    conv.conversor(data)
+    while True:
+        nomeFicheiro = input('Insira o path do ficheiro que quer converter : ')
+        conv = Conversor()
+
+        f = open(nomeFicheiro)
+        data = f.read()
+        data = conv.splitData(data)
+        conv.conversor(data)
 if __name__ == "__main__":
     main()
